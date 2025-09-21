d4352af8ffe7a90a92ec82750490a79e5a3b6ef2
Srikrishna <srikrishnawebdeveloper@gmail.com>
2025-09-21 20:29:00 +0530

    after adugment v)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+  port: 5175,
+  strictPort: true,
   },
 })
